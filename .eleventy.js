417c7f9cb03a24d0258dfcd5fb8fbd39176fbd83
pgm-minajaid <minajaid@student.arteveldehs.be>
2021-12-15 11:35:56 +0100

    changed path

--- .eleventy.js
+++ .eleventy.js
@@ -50,7 +50,7 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       data: "_data",
     },
-    pathPrefix: "/website",
+    pathPrefix: ".",
     templateFormats: ["html", "liquid", "md", "njk"],
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
